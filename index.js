7de530ced2aa1cf73fd1e9bf8c1e0e393dff1d87
YanC Lin <yonalin1@gmail.com>
2018-08-18 04:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name) {
   drivers.push('Ralph')
 }
+function destructivelyPrependDriver(name) {
+  drivers.unshift("Bob")
+}
